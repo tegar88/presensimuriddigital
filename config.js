b27dd42414c5af1fcfc29397097ab95ff48da485
Tegar Pambuditama <tegarpambuditama@gmail.com>
2026-09-03 12:11:43 +0700

    Add frontend configuration for app settings

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-
+/**
+ * KONFIGURASI FRONTEND
+ * GANTI API_URL dengan URL Apps Script Web App Anda.
+ */
+const APP_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXX/exec',
+  APP_NAME: 'ABSEN MURID',
+  SCHOOL_NAME: 'SMP NUSANTARA',
+  TAGLINE: 'Presensi sederhana, data terintegrasi.',
+  REFRESH_INTERVAL: 10000, // 10 detik untuk live feed
+  SESSION_KEY: 'absenmurid_session'
+};
